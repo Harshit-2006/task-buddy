833288935130e16ee53c4ff22bee63e80352908d
harshitbhatt <harshitbhatt8723@gmail.com>
2024-07-25 13:20:13 +0530

    Todo card component for displaying todo complete

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Footer from "./Footer";
 import Login from "./Login";
 import Navbar from "./Navbar";
 import SignUp from "./SignUp";
+import TodoCard from "./TodoCard";
 
-export {Footer,Login,Navbar,SignUp};
+export {Footer,Login,Navbar,SignUp,TodoCard};
