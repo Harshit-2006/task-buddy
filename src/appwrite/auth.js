53e06e1aeb29b39ac9f54601caa9606541d5be48
harshitbhatt <harshitbhatt8723@gmail.com>
2024-07-27 18:45:42 +0530

    add one more function for getting the current user

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -28,14 +28,17 @@ export class AuthService {
         return userAccount;
       }
     } catch (error) {
-      console.log("Appwrite service :: createAccount :: error ", error);
-      throw error;
+      console.log("Appwrite service :: createAccount :: error ", error.code);
+      return parseInt(error.code);
     }
   }
 
   // function to login user to the app
   async login({ email, password }) {
     try {
+      // const sessionId = window.localStorage.getItem("cookieFallback");
+      // //logging out the user if he is still logged in
+      // this.logout(sessionId);
       const session = await this.account.createEmailPasswordSession(
         email,
         password
@@ -44,21 +47,32 @@ export class AuthService {
       return session;
     } catch (error) {
       console.log("Appwrite service :: login :: error ", error);
-      throw error;
+      return parseInt(error.code);
     }
   }
 
   // function to logout the user
   async logout() {
     try {
-      await this.account.deleteSession();
+      await this.account.deleteSessions();
     } catch (error) {
       console.log("Appwrite service :: logout :: error ", error);
+      return parseInt(error.code);
+    }
+  }
+
+  // function to get the info of the currently login user
+  async getCurrentUser() {
+    try {
+      const user = await this.account.get();
+      console.log("Current user:", user);
+      return user;
+    } catch (error) {
+      console.log("Appwrite service :: getCurrentUser :: error", error);
       throw error;
     }
   }
 }
-
-const authService=new AuthService();
+const authService = new AuthService();
 
 export default authService;
